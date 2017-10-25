24fb305843225ea6f89d08da5ef77f2520650c42
Rich Bell <bellrichm@gmail.com>
2017-10-25 16:18:04 -0400

    Add build step when running code coverage

--- api/test/coverage.cmd
+++ api/test/coverage.cmd
@@ -4,6 +4,8 @@ set MSPEC=%HOMEPATH%\.nuget\packages\Machine.Specifications.Runner.Console\0.9.3
 set OPENCOVER=%HOMEPATH%\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe
 set REPORTGENERATOR=%HOMEPATH%\.nuget\packages\ReportGenerator\3.0.2\tools\ReportGenerator.exe
 
+dotnet build BellRichM.Weather.Api.Test/BellRichM.Weather.Api.Test.csproj
+
 rem this runs mspec standalone, not needed because it is run in OpenCover
 rem %MSPEC% --html coverage\mspec --xml coverage\mspec\mspec.xml BellRichM.Weather.Api.Test\bin\Debug\net462\BellRichM.Weather.Api.Test.dll
 
