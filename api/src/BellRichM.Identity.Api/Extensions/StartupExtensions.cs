56a6afec9c66b1abc7c96f9ac97271b048bc809e
Rich Bell <bellrichm@gmail.com>
2017-11-08 12:25:44 -0500

    enable config files close #1

--- api/src/BellRichM.Identity.Api/Extensions/StartupExtensions.cs
+++ api/src/BellRichM.Identity.Api/Extensions/StartupExtensions.cs
@@ -2,6 +2,7 @@ using System;
 using System.Text;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using BellRichM.Identity.Api.Data;
 
@@ -9,11 +10,10 @@ namespace BellRichM.Identity.Api.Extensions
 {
     public static class StartupExtensions
     {
-         public static void AddIdentityServices(this IServiceCollection services)
+        public static void AddIdentityServices(this IServiceCollection services, IConfigurationRoot Configuration)
         {
-            // TODO: Move connection string to config file
             services.AddDbContext<IdentityDbContext>(options =>
-                options.UseSqlite("DataSource=Data/Identity.db"));
+                options.UseSqlite(Configuration.GetConnectionString("(identityDb)")));
 
             services.AddIdentity<User, Role>()
                 .AddEntityFrameworkStores<IdentityDbContext>()
--- api/src/BellRichM.Weather.Web/Startup.cs
+++ api/src/BellRichM.Weather.Web/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using BellRichM.Identity.Api.Extensions;
 
@@ -12,11 +13,23 @@ namespace BellRichM.Weather.Api
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddIdentityServices();
+            services.AddIdentityServices(Configuration);
             services.AddMvc();
         }
 
