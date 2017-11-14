b3554071b4abf24caf834a12f91bc112bd04280d
Rich Bell <bellrichm@gmail.com>
2017-11-14 10:50:12 -0500

    code coverage for multiple projects

--- api/test/coverage.cmd
+++ api/test/coverage.cmd
@@ -4,11 +4,11 @@ set MSPEC=%HOMEPATH%\.nuget\packages\Machine.Specifications.Runner.Console\0.9.3
 set OPENCOVER=%HOMEPATH%\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe
 set REPORTGENERATOR=%HOMEPATH%\.nuget\packages\ReportGenerator\3.0.2\tools\ReportGenerator.exe
 
-dotnet build BellRichM.Weather.Api.Test/BellRichM.Weather.Api.Test.csproj
+dotnet build -f net462
 
 rem this runs mspec standalone, not needed because it is run in OpenCover
-rem %MSPEC% --html coverage\mspec --xml coverage\mspec\mspec.xml BellRichM.Weather.Api.Test\bin\Debug\net462\BellRichM.Weather.Api.Test.dll
+rem %MSPEC% --html coverage\mspec --xml coverage\mspec\mspec.xml BellRichM.Weather.Api.Test\bin\Debug\net462\BellRichM.Weather.Api.Test.dll BellRichM.Identity.Api.Test\bin\Debug\net462\BellRichM.Identity.Api.Test.dll
 
-%OPENCOVER% -register:user -output:coverage\opencover\opencover.xml -filter:"+[BellRichM*]* -[*.Test]*" -target:%MSPEC% -targetargs:"--html coverage\mspec --xml coverage\mspec\mspec.xml BellRichM.Weather.Api.Test\bin\Debug\net462\BellRichM.Weather.Api.Test.dll"
+%OPENCOVER% -register:user -output:coverage\opencover\opencover.xml -filter:"+[BellRichM*]* -[*.Test]*" -target:%MSPEC% -targetargs:"--html coverage\mspec --xml coverage\mspec\mspec.xml BellRichM.Weather.Api.Test\bin\Debug\net462\BellRichM.Weather.Api.Test.dll BellRichM.Identity.Api.Test\bin\Debug\net462\BellRichM.Identity.Api.Test.dll"
 
 %REPORTGENERATOR% -reporttypes:Html;XmlSummary;Xml -reports:coverage\opencover\opencover.xml -targetdir:coverage\report -historydir:coverage\report\history
