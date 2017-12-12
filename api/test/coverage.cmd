1dbe2c40cec962d3dcabb034278b63387d53bc78
Rich Bell <bellrichm@gmail.com>
2017-12-12 11:49:36 -0500

    get source to show on coveralls

--- api/test/coverage.cmd
+++ api/test/coverage.cmd
@@ -1,9 +1,14 @@
-cd c:\projects\weather\api\test
 
 set MSPEC=%HOMEPATH%\.nuget\packages\Machine.Specifications.Runner.Console\0.9.3\tools\mspec-clr4.exe
 set OPENCOVER=%HOMEPATH%\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe
 set COVERALLS=%HOMEPATH%\.nuget\packages\coveralls.io\1.4.2\tools\coveralls.net.exe
 
-%OPENCOVER% -register:user -output:opencover.xml -filter:"+[BellRichM*]* -[*]*.Migrations.* -[*.Test]*" -excludebyattribute:BellRichM.Attribute.CodeCoverage.ExcludeFromCodeCoverageAttribute -target:%MSPEC% -targetargs:"--xml .\mspec.xml BellRichM.Weather.Api.Test\bin\Debug\net462\BellRichM.Weather.Api.Test.dll BellRichM.Identity.Api.Test\bin\Debug\net462\BellRichM.Identity.Api.Test.dll"
+set TESTDIR=api\test\
+set TESTDLLS=
+set TESTDLLS=%TESTDLLS% %TESTDIR%BellRichM.Weather.Api.Test\bin\Debug\net462\BellRichM.Weather.Api.Test.dll
+set TESTDLLS=%TESTDLLS% %TESTDIR%BellRichM.Identity.Api.Test\bin\Debug\net462\BellRichM.Identity.Api.Test.dll
+
+
+%OPENCOVER% -register:user -output:opencover.xml -filter:"+[BellRichM*]* -[*]*.Migrations.* -[*.Test]*" -excludebyattribute:BellRichM.Attribute.CodeCoverage.ExcludeFromCodeCoverageAttribute -target:%MSPEC% -targetargs:"--xml .\mspec.xml %TESTDLLS%"
 
 %COVERALLS% --opencover opencover.xml --full-sources
