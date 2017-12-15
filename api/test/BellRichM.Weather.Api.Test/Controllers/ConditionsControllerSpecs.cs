948ffba77aa7f9a4ab833929fbed40a188f8a12e
Rich Bell <bellrichm@gmail.com>
2017-12-15 12:58:50 -0500

    code cleanup

--- api/test/BellRichM.Weather.Api.Test/Controllers/ConditionsControllerSpecs.cs
+++ api/test/BellRichM.Weather.Api.Test/Controllers/ConditionsControllerSpecs.cs
@@ -1,43 +1,41 @@
+using BellRichM.Weather.Api.Controllers;
 using FluentAssertions;
 using Machine.Specifications;
+using Microsoft.Extensions.Logging;
 using Moq;
+using System;
 
 using IT = Moq.It;
 using It = Machine.Specifications.It;
 
-using Microsoft.Extensions.Logging;
-using System;
-
-using BellRichM.Weather.Api.Controllers;
-
 namespace BellRichM.Weather.Api.Test.Controllers
 {
-	internal abstract class ConditionsControllerSpecs
-	{
-		[Subject("Get Conditions")]
-		internal class When_getting_current_condition
-		{
-			static ConditionsController conditionsController;
-			static Exception exception;
-			static Mock<ILogger<ConditionsController>> loggerMock;
+  internal abstract class ConditionsControllerSpecs
+  {
+    [Subject("Get Conditions")]
+    internal class When_getting_current_condition
+    {
+      static ConditionsController conditionsController;
+      static Exception exception;
+      static Mock<ILogger<ConditionsController>> loggerMock;
 
-			Establish context = () =>
-			{
-				loggerMock = new Mock<ILogger<ConditionsController>>();
-				conditionsController = new ConditionsController(loggerMock.Object);
-			};
+      Establish context = () =>
+      {
+        loggerMock = new Mock<ILogger<ConditionsController>>();
+        conditionsController = new ConditionsController(loggerMock.Object);
+      };
 
-			Because of = () =>
-				exception = Catch.Exception(() => conditionsController.Get());
+      Because of = () =>
+        exception = Catch.Exception(() => conditionsController.Get());
 
-			It should_throw_expected_exception = () =>
-				exception.ShouldBeOfExactType<NotImplementedException>();
-		}
+      It should_throw_expected_exception = () =>
+        exception.ShouldBeOfExactType<NotImplementedException>();
+    }
 
-		[Subject("Post Conditions")]
-		internal class When_adding_conditions
-		{
-			It should_do_something_cool;
-		}
-	}
+    [Subject("Post Conditions")]
+    internal class When_adding_conditions
+    {
+      It should_do_something_cool;
+    }
+  }
 }
