c20eb61ba93a9f280fca88003ce1627807d9b012
Rich Bell <bellrichm@gmail.com>
2017-12-23 14:17:53 -0500

    exclude tests from sonar analysis

--- appveyor/build.cmd
+++ appveyor/build.cmd
@@ -3,10 +3,11 @@ sonarqube.scanner.msbuild.exe begin ^
   /k:"weather" ^
   /o:"bellrichm" ^
   /v:%APPVEYOR_BUILD_VERSION% ^
+  /d:sonar.log.level=INFO ^
   /d:sonar.branch.name=%BRANCH_NAME% ^
   /d:sonar.host.url="https://sonarcloud.io" ^
   /d:sonar.login=%SONARQUBE_REPO_TOKEN%  ^
-  /d:sonar.exclusions="**/Migrations/*, **/obj/**/*"  ^
+  /d:sonar.exclusions="**/Migrations/*, **/obj/**/*, **/*Specs.*" ^
   /d:sonar.cs.opencover.reportsPaths="opencover.xml"
 
 dotnet build api\src\BellRichM.Weather.sln ^
