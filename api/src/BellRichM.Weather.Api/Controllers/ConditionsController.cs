8f0c2f0b846b8d009d3093bc27453b4c99059cb0
Rich Bell <bellrichm@gmail.com>
2018-01-24 14:30:38 -0500

    XML comments for Weather API

--- api/src/BellRichM.Weather.Api/Controllers/ConditionsController.cs
+++ api/src/BellRichM.Weather.Api/Controllers/ConditionsController.cs
@@ -5,16 +5,30 @@ using Microsoft.Extensions.Logging;
 
 namespace BellRichM.Weather.Api.Controllers
 {
+    /// <summary>
+    /// The conditions controller
+    /// </summary>
+    /// <seealso cref="Controller" />
     [Route("api/[controller]")]
-    public class ConditionsController
+    public class ConditionsController : Controller
     {
         private readonly ILogger _logger;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConditionsController"/> class.
+        /// </summary>
+        /// <param name="logger">The <see cref="ILogger{ConditionsController}"/>.</param>
         public ConditionsController(ILogger<ConditionsController> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Gets weather condistions.
+        /// </summary>
+        /// <returns>The <see cref="IEnumerable{String}"/></returns>
+        /// <exception cref="NotImplementedException">Not implemented.</exception>
+        /// <remarks>Not yet implemented.</remarks>
         [HttpGet]
         public IEnumerable<string> Get()
         {
@@ -22,6 +36,11 @@ namespace BellRichM.Weather.Api.Controllers
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Adds a new weather condition.
+        /// </summary>
+        /// <exception cref="NotImplementedException">Not Implemented</exception>
+        /// <remarks>Not yet implemented.</remarks>
         public void Post()
         {
             throw new NotImplementedException();
