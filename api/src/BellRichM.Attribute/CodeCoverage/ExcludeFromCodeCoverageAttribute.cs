3bfd0681bd8cb49fa1ea91b54032b3aa259b917e
Rich Bell <bellrichm@gmail.com>
2018-01-24 14:18:29 -0500

    XML comments for Attribute project

--- api/src/BellRichM.Attribute/CodeCoverage/ExcludeFromCodeCoverageAttribute.cs
+++ api/src/BellRichM.Attribute/CodeCoverage/ExcludeFromCodeCoverageAttribute.cs
@@ -2,6 +2,10 @@ using System;
 
 namespace BellRichM.Attribute.CodeCoverage
 {
+    /// <summary>
+    /// Use this attribute to exclude code from coverage analysis.
+    /// </summary>
+    /// <seealso cref="System.Attribute" />
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Constructor | AttributeTargets.Interface)]
     public class ExcludeFromCodeCoverageAttribute : System.Attribute
     {
