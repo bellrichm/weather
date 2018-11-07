8308d81a5415e07d5dad02eaf875aec1a2d60e5a
Rich Bell <bellrichm@gmail.com>
2018-11-07 10:57:48 -0500

    Add additional test dlls for coverage

--- appveyor/after_build.ps1
+++ appveyor/after_build.ps1
@@ -37,11 +37,15 @@ if ($env:UNIT_TEST -eq "NO" `
 
 if ($env:BUILD_PLATFORM-eq "Windows")
 {
-  $TESTDIR="api\test\"
-  $TESTDLLS = $TESTDIR + "BellRichM.Weather.Api.Test\bin\Debug\net462\BellRichM.Weather.Api.Test.dll "
-  $TESTDLLS = $TESTDLLS + $TESTDIR + "BellRichM.Identity.Api.Test\bin\Debug\net462\BellRichM.Identity.Api.Test.dll "
+  $TESTDIR = "api\test\"
+  $TESTDLLS = ""
+  
+  $TESTDLLS = $TESTDLLS + $TESTDIR + "BellRichM.Api.Test\bin\Debug\net462\BellRichM.Logging.Api.dll "
   $TESTDLLS = $TESTDLLS + $TESTDIR + "BellRichM.Configuration.Test\bin\Debug\net462\BellRichM.Configuration.Test.dll "
-  $TESTDLLS = $TESTDLLS + $TESTDIR + "BellRichM.Logging.Test\bin\Debug\net462\BellRichM.Logging.Test.dll "
+  $TESTDLLS = $TESTDLLS + $TESTDIR + "BellRichM.Exceptions.Test\bin\Debug\net462\BellRichM.Configuration.Test.dll "
+  $TESTDLLS = $TESTDLLS + $TESTDIR + "BellRichM.Identity.Api.Test\bin\Debug\net462\BellRichM.Identity.Api.Test.dll "  
+  $TESTDLLS = $TESTDLLS + $TESTDIR + "BellRichM.Logging.Test\bin\Debug\net462\BellRichM.Logging.Test.dll "  
+  $TESTDLLS = $TESTDLLS + $TESTDIR + "BellRichM.Weather.Api.Test\bin\Debug\net462\BellRichM.Weather.Api.Test.dll "
 
   $parms = ''
   $parms = $parms + '-register:user -output:opencover.xml '
