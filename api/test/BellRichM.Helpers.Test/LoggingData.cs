e02d241fe652f1a6857ead8368019a9606179930
Rich Bell <bellrichm@gmail.com>
2019-01-16 07:55:48 -0500

    Cleanup

--- api/test/BellRichM.Helpers.Test/LoggingData.cs
+++ api/test/BellRichM.Helpers.Test/LoggingData.cs
@@ -5,7 +5,9 @@ namespace BellRichM.Helpers.Test
     public class LoggingData
     {
         public int DebugTimes { get; set; }
+
         public int InformationTimes { get; set; }
+
         public IEnumerable<EventLoggingData> EventLoggingData { get; set; }
 
         public IEnumerable<string> ErrorLoggingMessages { get; set; }
