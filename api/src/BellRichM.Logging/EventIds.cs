ac88a6fba419302941edfed7a6705b728f41201f
Rich Bell <bellrichm@gmail.com>
2019-01-19 10:21:44 -0500

    cleanup

--- api/src/BellRichM.Logging/EventIds.cs
+++ api/src/BellRichM.Logging/EventIds.cs
@@ -170,7 +170,6 @@ namespace BellRichM.Logging
     /// The observation controller delete event id.
     /// </summary>
     ObservationsController_Delete,
-
     }
 }
 #pragma warning restore CA1707
