bd1e323f156b573c97806eded31e2c4b88c0d7d8
Rich Bell <bellrichm@gmail.com>
2019-01-21 16:03:39 -0500

    refactor some duplicate code

--- api/src/BellRichM.Weather.Api/Repositories/ObservationRepository.cs
+++ api/src/BellRichM.Weather.Api/Repositories/ObservationRepository.cs
@@ -163,26 +163,7 @@ INSERT INTO condition
      )
 ";
 
-            int rowCount;
-            var dbConnection = _observationDbProviderFactory.CreateConnection();
-            dbConnection.ConnectionString = _connectionString;
-            using (dbConnection)
-            {
-                var dbCommand = dbConnection.CreateCommand();
-                dbCommand.CommandText = statement;
-                using (dbCommand)
-                {
-                    AddParms(dbCommand, observation);
-
-                    dbConnection.Open();
-                    rowCount = await dbCommand.ExecuteNonQueryAsync().ConfigureAwait(true);
-                    dbCommand.Parameters.Clear();
-                }
-
-                dbConnection.Close();
-            }
-
-            return rowCount;
+            return await ExecuteNonQuery(statement, observation).ConfigureAwait(true);
         }
 
         /// <inheritdoc/>
@@ -239,25 +220,7 @@ UPDATE condition
         dateTime=@dateTime
 ";
 
-            int rowCount;
-            var dbConnection = _observationDbProviderFactory.CreateConnection();
-            dbConnection.ConnectionString = _connectionString;
-            using (dbConnection)
-            {
-                var dbCommand = dbConnection.CreateCommand();
-                dbCommand.CommandText = statement;
-                using (dbCommand)
-                {
-                    AddParms(dbCommand, observation);
-                    dbConnection.Open();
-                    rowCount = await dbCommand.ExecuteNonQueryAsync().ConfigureAwait(true);
-                    dbCommand.Parameters.Clear();
-                }
-
-                dbConnection.Close();
-            }
-
-            return rowCount;
+            return await ExecuteNonQuery(statement, observation).ConfigureAwait(true);
         }
 
         /// <inheritdoc/>
@@ -293,50 +256,69 @@ DELETE FROM condition
             return rowCount;
         }
 
-        private static void AddParms(DbCommand dbCommand, Observation observation)
+        private async Task<int> ExecuteNonQuery(string statement, Observation observation)
         {
-            dbCommand.AddParamWithValue("@year", observation.Year);
-            dbCommand.AddParamWithValue("@month", observation.Month);
-            dbCommand.AddParamWithValue("@day", observation.Day);
-            dbCommand.AddParamWithValue("@hour", observation.Hour);
-            dbCommand.AddParamWithValue("@minute", observation.Minute);
-            dbCommand.AddParamWithValue("@dateTime", observation.DateTime);
-            dbCommand.AddParamWithValue("@usUnits", observation.USUnits);
-            dbCommand.AddParamWithValue("@interval", observation.Interval);
-            dbCommand.AddParamWithValue("@barometer", observation.Barometer);
-            dbCommand.AddParamWithValue("@pressure", observation.Pressure);
-            dbCommand.AddParamWithValue("@altimeter", observation.Altimeter);
-            dbCommand.AddParamWithValue("@outTemp", observation.OutsideTemperature);
-            dbCommand.AddParamWithValue("@outHumidity", observation.OutsideHumidity);
-            dbCommand.AddParamWithValue("@windSpeed", observation.WindSpeed);
-            dbCommand.AddParamWithValue("@windDir", observation.WindDirection);
-            dbCommand.AddParamWithValue("@windGust", observation.WindGust);
-            dbCommand.AddParamWithValue("@windGustDir", observation.WindGustDirection);
-            dbCommand.AddParamWithValue("@rainRate", observation.RainRate);
-            dbCommand.AddParamWithValue("@rain", observation.Rain);
-            dbCommand.AddParamWithValue("@dewpoint", observation.DewPoint);
-            dbCommand.AddParamWithValue("@windchill", observation.Windchill);
-            dbCommand.AddParamWithValue("@heatindex", observation.HeatIndex);
-            dbCommand.AddParamWithValue("@ET", observation.Evapotranspiration);
-            dbCommand.AddParamWithValue("@radiation", observation.Radiation);
-            dbCommand.AddParamWithValue("@UV", observation.Ultraviolet);
-            dbCommand.AddParamWithValue("@extraTemp1", observation.ExtraTemperature1);
-            dbCommand.AddParamWithValue("@extraTemp2", observation.ExtraTemperature2);
-            dbCommand.AddParamWithValue("@extraTemp3", observation.ExtraTemperature3);
-            dbCommand.AddParamWithValue("@soilTemp1", observation.SoilTemperature1);
-            dbCommand.AddParamWithValue("@soilTemp2", observation.SoilTemperature2);
-            dbCommand.AddParamWithValue("@soilTemp3", observation.SoilTemperature3);
-            dbCommand.AddParamWithValue("@soilTemp4", observation.SoilTemperature4);
-            dbCommand.AddParamWithValue("@leafTemp1", observation.LeafTemperature1);
-            dbCommand.AddParamWithValue("@leafTemp2", observation.LeafTemperature2);
-            dbCommand.AddParamWithValue("@extraHumid1", observation.ExtraHumidity1);
-            dbCommand.AddParamWithValue("@extraHumid2", observation.ExtraHumidity2);
-            dbCommand.AddParamWithValue("@soilMoist1", observation.SoilMoisture1);
-            dbCommand.AddParamWithValue("@soilMoist2", observation.SoilMoisture2);
-            dbCommand.AddParamWithValue("@soilMoist3", observation.SoilMoisture3);
-            dbCommand.AddParamWithValue("@soilMoist4", observation.SoilMoisture4);
-            dbCommand.AddParamWithValue("@leafWet1", observation.LeafWetness1);
-            dbCommand.AddParamWithValue("@leafWet2", observation.LeafWetness2);
+            int rowCount;
+            var dbConnection = _observationDbProviderFactory.CreateConnection();
+            dbConnection.ConnectionString = _connectionString;
+            using (dbConnection)
+            {
+                var dbCommand = dbConnection.CreateCommand();
+                dbCommand.CommandText = statement;
+                using (dbCommand)
+                {
+                    dbCommand.AddParamWithValue("@year", observation.Year);
+                    dbCommand.AddParamWithValue("@month", observation.Month);
+                    dbCommand.AddParamWithValue("@day", observation.Day);
+                    dbCommand.AddParamWithValue("@hour", observation.Hour);
+                    dbCommand.AddParamWithValue("@minute", observation.Minute);
+                    dbCommand.AddParamWithValue("@dateTime", observation.DateTime);
+                    dbCommand.AddParamWithValue("@usUnits", observation.USUnits);
+                    dbCommand.AddParamWithValue("@interval", observation.Interval);
+                    dbCommand.AddParamWithValue("@barometer", observation.Barometer);
+                    dbCommand.AddParamWithValue("@pressure", observation.Pressure);
+                    dbCommand.AddParamWithValue("@altimeter", observation.Altimeter);
+                    dbCommand.AddParamWithValue("@outTemp", observation.OutsideTemperature);
+                    dbCommand.AddParamWithValue("@outHumidity", observation.OutsideHumidity);
+                    dbCommand.AddParamWithValue("@windSpeed", observation.WindSpeed);
+                    dbCommand.AddParamWithValue("@windDir", observation.WindDirection);
+                    dbCommand.AddParamWithValue("@windGust", observation.WindGust);
+                    dbCommand.AddParamWithValue("@windGustDir", observation.WindGustDirection);
+                    dbCommand.AddParamWithValue("@rainRate", observation.RainRate);
+                    dbCommand.AddParamWithValue("@rain", observation.Rain);
+                    dbCommand.AddParamWithValue("@dewpoint", observation.DewPoint);
+                    dbCommand.AddParamWithValue("@windchill", observation.Windchill);
+                    dbCommand.AddParamWithValue("@heatindex", observation.HeatIndex);
+                    dbCommand.AddParamWithValue("@ET", observation.Evapotranspiration);
+                    dbCommand.AddParamWithValue("@radiation", observation.Radiation);
+                    dbCommand.AddParamWithValue("@UV", observation.Ultraviolet);
+                    dbCommand.AddParamWithValue("@extraTemp1", observation.ExtraTemperature1);
+                    dbCommand.AddParamWithValue("@extraTemp2", observation.ExtraTemperature2);
+                    dbCommand.AddParamWithValue("@extraTemp3", observation.ExtraTemperature3);
+                    dbCommand.AddParamWithValue("@soilTemp1", observation.SoilTemperature1);
+                    dbCommand.AddParamWithValue("@soilTemp2", observation.SoilTemperature2);
+                    dbCommand.AddParamWithValue("@soilTemp3", observation.SoilTemperature3);
+                    dbCommand.AddParamWithValue("@soilTemp4", observation.SoilTemperature4);
+                    dbCommand.AddParamWithValue("@leafTemp1", observation.LeafTemperature1);
+                    dbCommand.AddParamWithValue("@leafTemp2", observation.LeafTemperature2);
+                    dbCommand.AddParamWithValue("@extraHumid1", observation.ExtraHumidity1);
+                    dbCommand.AddParamWithValue("@extraHumid2", observation.ExtraHumidity2);
+                    dbCommand.AddParamWithValue("@soilMoist1", observation.SoilMoisture1);
+                    dbCommand.AddParamWithValue("@soilMoist2", observation.SoilMoisture2);
+                    dbCommand.AddParamWithValue("@soilMoist3", observation.SoilMoisture3);
+                    dbCommand.AddParamWithValue("@soilMoist4", observation.SoilMoisture4);
+                    dbCommand.AddParamWithValue("@leafWet1", observation.LeafWetness1);
+                    dbCommand.AddParamWithValue("@leafWet2", observation.LeafWetness2);
+
+                    dbConnection.Open();
+                    rowCount = await dbCommand.ExecuteNonQueryAsync().ConfigureAwait(true);
+                    dbCommand.Parameters.Clear();
+                }
+
+                dbConnection.Close();
+            }
+
+            return rowCount;
         }
     }
 }
