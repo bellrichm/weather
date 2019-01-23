e3744fd413c797b3ea4d67d8d239cb13658cdb2a
Rich Bell <bellrichm@gmail.com>
2019-01-23 15:44:44 -0500

    validate condition controller limit query parameter

---
+++ api/src/BellRichM.Weather.Api/Filters/ValidateConditionLimitAttribute.cs
@@ -0,0 +1,71 @@
+using BellRichM.Weather.Api.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace BellRichM.Weather.Api.Filters
+{
+      /// <summary>
+      /// Validates that the limit query parameter values.
+      /// </summary>
+    public class ValidateConditionLimitAttribute : TypeFilterAttribute
+    {
+      /// <summary>
+      /// Initializes a new instance of the <see cref="ValidateConditionLimitAttribute"/> class.
+      /// </summary>
+      public ValidateConditionLimitAttribute()
+        : base(typeof(ValidateConditionLimitAttributeImplementation))
+      {
+      }
+
+      private class ValidateConditionLimitAttributeImplementation : ActionFilterAttribute
+      {
+          private readonly string limitMissingMessage = "The query paramenter limit is required.";
+          private readonly string limitMinMessage = "The limit query paramenter must be greater than 0.";
+          private readonly string limitMaxMessage = "The limit query parameter must be less than or equal to {0}.";
+
+          private readonly string limitMissingCode = "LimitMissing";
+          private readonly string limitMinCode = "LimitMin";
+          private readonly string limitMaxCode = "LimitMax";
+
+          private readonly IConditionRepositoryConfiguration _conditonRepositoryConfiguration;
+
+          /// <summary>
+          /// Initializes a new instance of the <see cref="ValidateConditionLimitAttributeImplementation"/> class.
+          /// </summary>
+          /// <param name="conditionRepositoryConfiguration">The <see cref="IConditionRepositoryConfiguration"/>.</param>
+          public ValidateConditionLimitAttributeImplementation(IConditionRepositoryConfiguration conditionRepositoryConfiguration)
+          {
+            _conditonRepositoryConfiguration = conditionRepositoryConfiguration;
+          }
+
+          /// <inheritdoc/>
+          public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+          {
+            int? limit = null;
+            if (context.ActionArguments.ContainsKey("limit"))
+            {
+              limit = context.ActionArguments["limit"] as int?;
+            }
+
+            if (limit == null)
+            {
+              context.ModelState.AddModelError(limitMissingCode, limitMissingMessage);
+            }
+
+            if (limit <= 0)
+            {
+              context.ModelState.AddModelError(limitMinCode, limitMinMessage);
+            }
+
+            if (limit > _conditonRepositoryConfiguration.MaximumConditions)
+            {
+              context.ModelState.AddModelError(limitMaxCode, string.Format(CultureInfo.InvariantCulture, limitMaxMessage, _conditonRepositoryConfiguration.MaximumConditions));
+            }
+
+            await base.OnActionExecutionAsync(context, next).ConfigureAwait(true);
+          }
+      }
+    }
+}
---
+++ api/test/BellRichM.Weather.Api.Test/Filters/ValidateConditionLimitAttributeSpecs.cs
@@ -0,0 +1,253 @@
+using BellRichM.Helpers.Test;
+using BellRichM.Logging;
+using BellRichM.Weather.Api.Configuration;
+using BellRichM.Weather.Api.Filters;
+using FluentAssertions;
+using Machine.Specifications;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Routing;
+using Moq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Threading.Tasks;
+
+using IT = Moq.It;
+using It = Machine.Specifications.It;
+
+namespace BellRichM.Weather.Api.Test.Filters
+{
+    public class ValidateConditionLimitAttributeSpecs
+    {
+        protected const int ConfiguredLimit = 3;
+        protected static MethodInfo methodInfo;
+        protected static object validateConditionLimitAttributeImplementationTypeInstance;
+        protected static ActionContext actionContext;
+        protected static ActionExecutionDelegate actionExecutionDelegate;
+
+        protected static LoggingData loggingData;
+
+        protected static Mock<ILoggerAdapter<ValidateConditionLimitAttributeSpecs>> loggerMock;
+
+        protected static ValidateConditionLimitAttributeSpecs validateConditionLimitAttribute;
+
+        Establish context = () =>
+        {
+            // default to no logging
+            loggingData = new LoggingData
+            {
+                EventLoggingData = new List<EventLoggingData>(),
+                ErrorLoggingMessages = new List<string>()
+            };
+
+            loggerMock = new Mock<ILoggerAdapter<ValidateConditionLimitAttributeSpecs>>();
+
+            actionContext = new ActionContext
+                {
+                    HttpContext = Mock.Of<HttpContext>(),
+                    RouteData = Mock.Of<RouteData>(),
+                    ActionDescriptor = new ActionDescriptor(),
+                };
+
+            var config = new ConditionRepositoryConfiguration
+            {
+                MaximumConditions = ConfiguredLimit
+            };
+
+            Task<ActionExecutedContext> ActionExecute()
+            {
+              return Task.FromResult<ActionExecutedContext>(null);
+            }
+
+            actionExecutionDelegate = ActionExecute;
+
+            validateConditionLimitAttribute = new ValidateConditionLimitAttributeSpecs();
+            var validateConditionLimitAttributeType = typeof(ValidateConditionLimitAttribute);
+            var validateConditionLimitAttributeImplementationType = validateConditionLimitAttributeType.GetNestedType("ValidateConditionLimitAttributeImplementation", BindingFlags.NonPublic);
+
+            validateConditionLimitAttributeImplementationTypeInstance = Activator.CreateInstance(validateConditionLimitAttributeImplementationType, config);
+            methodInfo = validateConditionLimitAttributeImplementationType.GetMethod("OnActionExecutionAsync");
+        };
+    }
+
+    internal class When_limit_is_valid : ValidateConditionLimitAttributeSpecs
+    {
+        protected const int LimitQueryParameter = 2;
+        protected static object[] parametersArray;
+        protected static ActionExecutingContext actionExecutingContext;
+
+        Establish context = () =>
+        {
+            actionExecutingContext = new ActionExecutingContext(
+                actionContext,
+                new List<IFilterMetadata>(),
+                new Dictionary<string, object>()
+                {
+                    { "limit", LimitQueryParameter }
+                },
+                Mock.Of<Controller>());
+
+            parametersArray = new object[] { actionExecutingContext, actionExecutionDelegate };
+        };
+
+        Because of = () =>
+            methodInfo.Invoke(validateConditionLimitAttributeImplementationTypeInstance, parametersArray);
+
+#pragma warning disable 169
+        Behaves_like<LoggingBehaviors<ValidateConditionLimitAttributeSpecs>> correct_logging;
+#pragma warning restore 169
+
+        It should_have_one_ModelStateDictionary_entry = () =>
+            actionExecutingContext.ModelState.Count.Should().Equals(0);
+    }
+
+    internal class When_limit_is_missing : ValidateConditionLimitAttributeSpecs
+    {
+        protected const string ErrorMessage = "The query paramenter limit is required.";
+        protected const string ErrorCode = "LimitMin";
+        protected static object[] parametersArray;
+        protected static ActionExecutingContext actionExecutingContext;
+
+        Establish context = () =>
+        {
+            actionExecutingContext = new ActionExecutingContext(
+                actionContext,
+                new List<IFilterMetadata>(),
+                new Dictionary<string, object>(),
+                Mock.Of<Controller>());
+
+            parametersArray = new object[] { actionExecutingContext, actionExecutionDelegate };
+        };
+
+        Because of = () =>
+            methodInfo.Invoke(validateConditionLimitAttributeImplementationTypeInstance, parametersArray);
+
+#pragma warning disable 169
+        Behaves_like<LoggingBehaviors<ValidateConditionLimitAttributeSpecs>> correct_logging;
+#pragma warning restore 169
+
+        It should_have_one_ModelStateDictionary_entry = () =>
+            actionExecutingContext.ModelState.Count.Should().Equals(1);
+
+        It should_have_correct_key = () =>
+            actionContext.ModelState.First().Key.Should().Equals(ErrorCode);
+
+        It should_have_one_ModelError = () =>
+        {
+            var modelErrors = actionContext.ModelState.First().Value.Errors;
+            modelErrors.Count.Should().Equals(1);
+        };
+
+        It should_have_correct_message = () =>
+        {
+            var errorMessage = actionContext.ModelState.First().Value.Errors[0].ErrorMessage;
+            errorMessage.Should().BeEquivalentTo(ErrorMessage);
+        };
+    }
+
+    internal class When_limit_is_smaller_than_minimum_allowed : ValidateConditionLimitAttributeSpecs
+    {
+        protected const int LimitQueryParameter = 0;
+        protected const string ErrorMessage = "The limit query paramenter must be greater than 0.";
+        protected const string ErrorCode = "LimitMin";
+        protected static object[] parametersArray;
+        protected static ActionExecutingContext actionExecutingContext;
+
+        Establish context = () =>
+        {
+            actionExecutingContext = new ActionExecutingContext(
+                actionContext,
+                new List<IFilterMetadata>(),
+                new Dictionary<string, object>()
+                {
+                    { "limit", LimitQueryParameter }
+                },
+                Mock.Of<Controller>());
+
+            parametersArray = new object[] { actionExecutingContext, actionExecutionDelegate };
+        };
+
+        Because of = () =>
+            methodInfo.Invoke(validateConditionLimitAttributeImplementationTypeInstance, parametersArray);
+
+#pragma warning disable 169
+        Behaves_like<LoggingBehaviors<ValidateConditionLimitAttributeSpecs>> correct_logging;
+#pragma warning restore 169
+
+        It should_have_one_ModelStateDictionary_entry = () =>
+            actionExecutingContext.ModelState.Count.Should().Equals(1);
+
+        It should_have_correct_key = () =>
+            actionContext.ModelState.First().Key.Should().Equals(ErrorCode);
+
+        It should_have_one_ModelError = () =>
+        {
+            var modelErrors = actionContext.ModelState.First().Value.Errors;
+            modelErrors.Count.Should().Equals(1);
+        };
+
+        It should_have_correct_message = () =>
+        {
+            var errorMessage = actionContext.ModelState.First().Value.Errors[0].ErrorMessage;
+            errorMessage.Should().BeEquivalentTo(ErrorMessage);
+        };
+    }
+
+    internal class When_limit_is_bigger_than_maximimum_allowed : ValidateConditionLimitAttributeSpecs
+    {
+        protected const int LimitQueryParameter = 5;
+        protected const string ErrorMessage = "The limit query parameter must be less than or equal to {0}.";
+        protected const string ErrorCode = "LimitMax";
+        protected static object[] parametersArray;
+        protected static ActionExecutingContext actionExecutingContext;
+
+        Establish context = () =>
+        {
+            actionExecutingContext = new ActionExecutingContext(
+                actionContext,
+                new List<IFilterMetadata>(),
+                new Dictionary<string, object>()
+                {
+                    { "limit", LimitQueryParameter }
+                },
+                Mock.Of<Controller>());
+
+            parametersArray = new object[] { actionExecutingContext, actionExecutionDelegate };
+        };
+
+        Because of = () =>
+            methodInfo.Invoke(validateConditionLimitAttributeImplementationTypeInstance, parametersArray);
+
+#pragma warning disable 169
+        Behaves_like<LoggingBehaviors<ValidateConditionLimitAttributeSpecs>> correct_logging;
+#pragma warning restore 169
+
+        It should_have_one_ModelStateDictionary_entry = () =>
+            actionExecutingContext.ModelState.Count.Should().Equals(1);
+
+        It should_have_correct_key = () =>
+            actionContext.ModelState.First().Key.Should().Equals(ErrorCode);
+
+        It should_have_one_ModelError = () =>
+        {
+            var modelErrors = actionContext.ModelState.First().Value.Errors;
+            modelErrors.Count.Should().Equals(1);
+        };
+
+        It should_have_correct_message = () =>
+        {
+            var errorMessage = actionContext.ModelState.First().Value.Errors[0].ErrorMessage;
+            errorMessage.Should().BeEquivalentTo(string.Format(CultureInfo.InvariantCulture, ErrorMessage, ConfiguredLimit));
+        };
+    }
+}
