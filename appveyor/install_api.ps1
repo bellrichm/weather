86811270d589583ef90690a6ccd01311907b641b
Rich Bell <bellrichm@gmail.com>
2020-02-17 18:18:25 -0500

    coveralls path

--- appveyor/install_api.ps1
+++ appveyor/install_api.ps1
@@ -6,5 +6,5 @@ if ($env:BUILDTYPE -ne "LOCAL")
 {
     $env:TOOLDIR = "buildtools/"
     dotnet tool install dotnet-reportgenerator-globaltool --tool-path buildtools
-    dotnet tool install coveralls.net --version 1.0.0 --tool-path tools
+    dotnet tool install coveralls.net --version 1.0.0 --tool-path buildtools
 }
