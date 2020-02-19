64659776938dd070246f6e3d2b0bfed055f1feaa
Rich Bell <bellrichm@gmail.com>
2020-02-19 11:49:37 -0500

    display stderr to console

--- appveyor/after_build_test_unit_app.ps1
+++ appveyor/after_build_test_unit_app.ps1
@@ -14,14 +14,16 @@ if ($env:UNIT_TEST_APP -ne "NO")
   # stderr on appveyor workaround
   $cmd = "npm run-script ng build -- --progress=false --aot=true --prod=true 2>t.txt"
   RunCmd $cmd
-  "t.txt content is:"
+  "t.txt content beg:"
   Get-Content t.txt
+  "t.txt content end:"
 
   # stderr on appveyor workaround
   $cmd = "npm run-script ng test -- --progress=false --watch=false --browsers ChromeHeadless --code-coverage 2>t.txt"
   RunCmd $cmd
-  "t.txt content is:"
+  "t.txt content beg:"
   Get-Content t.txt
+  "t.txt content end:"
   set-location ..
 }
 
