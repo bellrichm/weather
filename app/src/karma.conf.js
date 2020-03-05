8229693652ef39d89f27207e65f26f5f43c66f71
Rich Bell <bellrichm@gmail.com>
2020-03-05 14:44:07 -0500

    junit output

--- app/src/karma.conf.js
+++ app/src/karma.conf.js
@@ -10,17 +10,23 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-junit-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    junitReporter: {
+      outputDir: require('path').join(__dirname, '../output'),
+      outputFile: 'junit.xml',
+      useBrowserName: false
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
