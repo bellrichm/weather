5cec93d50a8ff48f8d8c8e14fa1cdb5caf497089
Rich Bell <bellrichm@gmail.com>
2020-03-19 10:21:41 -0400

    accept type attribute tests

--- api/test/BellRichM.Attribute.Test/Selector/AcceptTypeAttributeSpecs.cs
+++ api/test/BellRichM.Attribute.Test/Selector/AcceptTypeAttributeSpecs.cs
@@ -1,18 +1,107 @@
+using BellRichM.Attribute.Selector;
 using BellRichM.TestRunner;
 using Machine.Specifications;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Moq;
+
+using It = Machine.Specifications.It;
 
 namespace BellRichM.Attribute.Test.Selector
 {
     public class AcceptTypeAttributeSpecs
     {
+    }
+
+    internal class When_request_has_no_accept_header : AcceptTypeAttributeSpecs
+    {
+        protected static DefaultHttpContext httpContext;
+        protected static RouteContext routeContext;
+
+        protected static AcceptTypeAttribute acceptTypeAttribute;
+
+        protected static bool result;
+
         protected Establish context = () =>
-        { };
+        {
+            httpContext = new DefaultHttpContext();
+            routeContext = new RouteContext(httpContext);
 
-        protected Because of = () =>
-        { };
+            acceptTypeAttribute = new AcceptTypeAttribute("foobarx");
+        };
 
-        protected It should = () =>
-        { };
+        protected Because of = () => result = acceptTypeAttribute.IsValidForRequest(routeContext, null);
+
+        protected It should_return_false = () => result.ShouldBeFalse();
+    }
+
+    internal class When_request_has_accept_header_with_incorrect_value : AcceptTypeAttributeSpecs
+    {
+        protected static DefaultHttpContext httpContext;
+        protected static RouteContext routeContext;
+
+        protected static AcceptTypeAttribute acceptTypeAttribute;
+
+        protected static bool result;
+
+        protected Establish context = () =>
+        {
+            httpContext = new DefaultHttpContext();
+            httpContext.Request.Headers["Accept"] = "foobar";
+            routeContext = new RouteContext(httpContext);
+
+            acceptTypeAttribute = new AcceptTypeAttribute("foobarx");
+        };
+
+        protected Because of = () => result = acceptTypeAttribute.IsValidForRequest(routeContext, null);
+
+        protected It should_return_false = () => result.ShouldBeFalse();
+    }
+
+    internal class When_request_has_accept_header_with_correct_value : AcceptTypeAttributeSpecs
+    {
+        protected static DefaultHttpContext httpContext;
+        protected static RouteContext routeContext;
+
+        protected static AcceptTypeAttribute acceptTypeAttribute;
+
+        protected static bool result;
+
+        protected Establish context = () =>
+        {
+            httpContext = new DefaultHttpContext();
+            httpContext.Request.Headers["Accept"] = "foobar";
+            routeContext = new RouteContext(httpContext);
+
+            acceptTypeAttribute = new AcceptTypeAttribute("foobar");
+        };
+
+        protected Because of = () => result = acceptTypeAttribute.IsValidForRequest(routeContext, null);
+
+        protected It should_return_true = () => result.ShouldBeTrue();
+    }
+
+    internal class When_request_has_accept_header_with_format_information : AcceptTypeAttributeSpecs
+    {
+        protected static DefaultHttpContext httpContext;
+        protected static RouteContext routeContext;
+
+        protected static AcceptTypeAttribute acceptTypeAttribute;
+
+        protected static bool result;
+
+        protected Establish context = () =>
+        {
+            httpContext = new DefaultHttpContext();
+            httpContext.Request.Headers["Accept"] = "foobar+json";
+            routeContext = new RouteContext(httpContext);
+
+            acceptTypeAttribute = new AcceptTypeAttribute("foobar");
+        };
+
+        protected Because of = () => result = acceptTypeAttribute.IsValidForRequest(routeContext, null);
+
+        protected It should_return_true = () => result.ShouldBeTrue();
     }
 
     class Program
