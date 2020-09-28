0a3fa7034bd1636b184067af2dcc289bf5ddcaea
Rich Bell <bellrichm@gmail.com>
2020-09-28 16:43:24 -0400

    appveyor chrome headless timeout attempt #2

--- appveyor/after_build_test_unit_app.ps1
+++ appveyor/after_build_test_unit_app.ps1
@@ -14,7 +14,7 @@ set-location app
 
 # stderr on appveyor workaround
 # $cmd = "npm run-script ng test -- --progress=false --watch=false --browsers ChromeHeadless --code-coverage --source-map=false 2>t.txt"
-$cmd = "npm run-script ng test -- --progress=false --watch=false --browsers ChromeHeadless --code-coverage --source-map=false"
+$cmd = "npm run-script ng test -- --progress=false --watch=false --browsers ChromeHeadless --code-coverage --source-map=false --no-sandbox"
 RunCmd $cmd
 "t.txt content beg:"
 Get-Content t.txt
