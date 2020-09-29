0fbabbcfeedf31e2374609f1029acebe8b12f88e
Rich Bell <bellrichm@gmail.com>
2020-09-29 09:36:24 -0400

    appveyor chrome headless timeout #19 chromebin

--- appveyor/after_build_test_unit_app.ps1
+++ appveyor/after_build_test_unit_app.ps1
@@ -13,28 +13,30 @@ if ($env:UNIT_TEST_APP -EQ "NO" `
 set-location app
 
 $blockRdp = $true
-Invoke-Expression ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))
+#Invoke-Expression ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))
 
 #$cmd = "dir 'C:\Program Files (x86)'"
 #RunCmd $cmd
 
 $cmd = "dir 'C:\Program Files (x86)\Google'"
-RunCmd $cmd
+#RunCmd $cmd
 
 $cmd = "dir 'C:\Program Files (x86)\Google\Chrome'"
-RunCmd $cmd
+#endregionRunCmd $cmd
 
 $cmd = "dir 'C:\Program Files (x86)\Google\Chrome\Application'"
-RunCmd $cmd
+#RunCmd $cmd
 
 $cmd = "dir 'C:\Program Files (x86)\Google\Chrome\Application'"
-RunCmd $cmd
+#RunCmd $cmd
 
 $cmd = "dir 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'"
-RunCmd $cmd
+#RunCmd $cmd
 
 $cmd = "'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe --user-data-dir=C:\Users\appveyor\AppData\Local\Temp\1\karma-45328791 --no-default-browser-check --no-first-run --disable-default-apps --disable-popup-blocking --disable-translate --disable-background-timer-throttling --disable-renderer-backgrounding --disable-device-discovery-notifications --disable-web-security --disable-gpu --enable-logging=stdout  --v=1 --no-sandbox http://localhost:9876/?id=45328791 --headless --disable-gpu --remote-debugging-port=9222'"
-RunCmd $cmd
+##RunCmd $cmd
+
+$Env:CHROME_BIN = "C:\Program Files\Google\Chrome\Application\chrome.exe"
 
 # stderr on appveyor workaround
 # $cmd = "npm run-script ng test -- --progress=false --watch=false --browsers ChromeHeadless --code-coverage --source-map=false 2>t.txt"
