4726568073745f1d804aaa9ec6c3cd8fe5dfaeb5
Rich Bell <bellrichm@gmail.com>
2020-09-29 12:50:31 -0400

    first attempt at catchin an exception

--- appveyor/after_build_test_unit_app.ps1
+++ appveyor/after_build_test_unit_app.ps1
@@ -10,18 +10,26 @@ if ($env:UNIT_TEST_APP -EQ "NO" `
   return
 }
 
-set-location app
-
-# stderr on appveyor workaround
-$cmd = "npm run-script ng test -- --progress=false --watch=false --browsers ChromeHeadless --code-coverage 2>t.txt"
-RunCmd $cmd
-"t.txt content beg:"
-Get-Content t.txt
-"t.txt content end:"
-set-location ..
-
-if ($env:BUILDTYPE -ne 'LOCAL')
+try
 {
-  $wc = New-Object 'System.Net.WebClient'
-  $wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path ./app/output/junit.xml))
+  set-location app
+  
+  # stderr on appveyor workaround
+  $cmd = "npm run-script ng test -- --progress=false --watch=false --browsers ChromeHeadless --code-coverage 2>t.txt"
+  RunCmd $cmd
+  "t.txt content beg:"
+  Get-Content t.txt
+  "t.txt content end:"
+  set-location ..
+  
+  if ($env:BUILDTYPE -ne 'LOCAL')
+  {
+    $wc = New-Object 'System.Net.WebClient'
+    $wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path ./app/output/junit.xml))
+  }
+}
+catch
+{
+  Write-Host $_
+  return 1
 }
