a4f35425a8ab3270fcd2a9bd98e1a11b1f5df239
Rich Bell <bellrichm@gmail.com>
2020-09-28 21:20:54 -0400

    appveyor chrome headless timeout #5

--- app/src/karma.conf.js
+++ app/src/karma.conf.js
@@ -38,7 +38,11 @@ module.exports = function (config) {
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
+        flags: [
+          '--disable-web-security',
+          '--disable-gpu',
+          '--no-sandbox'
+        ]
       }
     },
     singleRun: false
